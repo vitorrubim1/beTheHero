711525a0413bb0338da780a69cec49f2f1881245
Vitor <vitorrubim1@hotmail.com>
2020-03-28 17:55:42 -0300

    feat: adicionando variável de ambiente pra URL da API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //PARAMETROS
-    baseURL: 'http://localhost:3333', //BACKEND
+    baseURL: process.env.REACT_APP_API_URL //BACKEND
 })
 
 export default api;
